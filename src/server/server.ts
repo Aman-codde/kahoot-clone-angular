1695bbfcb83f1b17b46055b3024b2899233dd2c3
Aman <amandeepk221@gmail.com>
2022-04-07 06:09:01 -0700

    ref: remove empty lines, make code more readable

--- src/server/server.ts
+++ src/server/server.ts
@@ -11,14 +11,9 @@ import { QuizzModel } from "./schemas/quizzes.schema.js";
 import { QuestionsModel } from "./schemas/questions.schemas.js";
 import { PlayerModel } from "./schemas/players.schema.js"
 import path from 'path';
-
-
 import dotenv from 'dotenv';
+
 dotenv.config();
-
-
-
-
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -28,7 +23,6 @@ export let io = new Server(server, {
 	cors: { origin: ["http://localhost:4200"] },
   });
   
-
 app.use(cors());
 app.use(express.static("public"));
 
@@ -40,17 +34,11 @@ mongoose
 	})
 	.catch((err) => console.log("Failed to Connect to DB", err));
 
-
-	console.log(process.env.MONGO_URI)
-
 //**********************************************************************
 
 const __dirname = path.resolve();
-
 const clientPath = path.join(__dirname, '/dist/client');
 app.use(express.static(clientPath));
-
-
 
 app.get("*", function (req, res) {
 	const filePath = path.join(__dirname, '/dist/client/index.html');
@@ -58,14 +46,11 @@ app.get("*", function (req, res) {
 	res.sendFile(filePath);
   });
 
-
-
+//**********************************************************************
   
-
 const players: Player[] = [];
 const Quizzes: Quiz[] = [];
 const question: Questions[] = [];
-
 
 //Connect Socket.io and Add players
 io.on("connection", (socket) => {
@@ -80,7 +65,6 @@ io.on("connection", (socket) => {
 		socket.emit("route", "lobby");
 	}
 
-
 	//Get Quizzes
 	socket.on("get quizzes", () => {
 		QuizzModel.find().then((data) => {
@@ -88,87 +72,67 @@ io.on("connection", (socket) => {
 		});
 	});
    
+ 	//Select Quiz
+ 	socket.on('select quiz', (quizz:Quiz)=>{
+		QuestionsModel.find({quizId:quizz.quizId}).then((questions)=>{
+			selectQuiz(questions)
+			console.log(JSON.stringify(game,null,4));
+			socket.emit("route", "lobby");      
+		})
+ 	})
 
-
- //Select Quiz
- socket.on('select quiz', (quizz:Quiz)=>{
-	 QuestionsModel.find({quizId:quizz.quizId}).then((questions)=>{
-		selectQuiz(questions)
-		console.log(JSON.stringify(game,null,4));
-		socket.emit("route", "lobby");      
-	 })
+	//Add Name
+	socket.on("add name", (name) => {
+   		name = addName(name, socket.id)
+		console.log(game);
+ 		io.emit("player added name", game.players);
+	})
  
- })
-
-
-
- //Add Name
- socket.on("add name", (name) => {
-   name = addName(name, socket.id)
-	console.log(game);
- io.emit("player added name", game.players);
- })
- 
- 
-
-
- //Host Add name
-  socket.on("add name",(name)=>{
+ 	//Host Add name
+  	socket.on("add name",(name)=>{
 	  if(player.host){
 		addName(name, socket.id)
-	//console.log(game);
-	socket.emit("route", "lobby");
-    io.emit("player added name", game.players);  
+		//console.log(game);
+		socket.emit("route", "lobby");
+    	io.emit("player added name", game.players);  
+		}
+  	})
 
-	  }
-  } )
+	// Take all players to Quiz
+	socket.on('go-to-quiz', ()=>{
+		io.emit('route','question-page')
+	})
 
+	//Request Question
+	socket.on("request-question", ()=>{
+		const question = getQuestion()
+		socket.emit("data-question", question)
+	})
 
+	//check for the answer
+	socket.on("answer-question",(answer)=>{
+   		if(answer){
+			const allAnswer = answerQuestion( socket.id, answer)
+			if(allAnswer){
+				io.emit('route','leader-board')
+			}
+		}
+	})
 
-// Take all players to Quiz
-socket.on('go-to-quiz', ()=>{
-	io.emit('route','question-page')
+	//get players with points
+ 	socket.on("request-gamePlayers", ()=>{
+		const allPlayers = getPlayers()
+		socket.emit("allPlayer-data", allPlayers)
+	 	console.log(allPlayers)
+	})
 
-})
+	// Go to next question 
+	socket.on('go-to-next-question', ()=>{  
+		io.emit('route','question-page')
+	})
 
+}); //This is the end os socket connection
 
- //Request Question
-socket.on("request-question", ()=>{
-const question = getQuestion()
-socket.emit("data-question", question)
-
-  })
-
-
-
-//check for the answer
-socket.on("answer-question",(answer)=>{
-   if(answer){
-	const allAnswer = answerQuestion( socket.id, answer)
-	if(allAnswer){
-		io.emit('route','leader-board')
-	}
-}})
-
-
- //get players with points
- socket.on("request-gamePlayers", ()=>{
-	const allPlayers = getPlayers()
-	socket.emit("allPlayer-data", allPlayers)
-	 console.log(allPlayers)
-	  })
-
-
-// Go to next question 
-socket.on('go-to-next-question', ()=>{  
-	io.emit('route','question-page')
-})
-
-
-
-
-
-}); //This is the end
 
 server.listen(PORT, () => {
 	console.log("listening on http://localhost:" + PORT);
